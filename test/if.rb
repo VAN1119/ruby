eea8acb47acaa6c5016aca8f0abafef720e65953
VAN1119 <bnaoya@yahoo.co.jp>
2022-02-21 10:22:36 +0900

    new file

---
+++ test/if.rb
@@ -0,0 +1,13 @@
+a = "apple"
+b = "banana"
+x = 1
+y = 2
+z = 3
+
+if a == "apple" then
+    puts "好きな果物は#{a}です"
+elsif a == "banana" then
+    puts "好きな果物は#{a}です"
+else
+    puts "好きな果物はありません"
+end
