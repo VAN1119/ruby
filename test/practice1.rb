d1244b02cf63b4ad93170bed504e45a81f562e52
VAN1119 <bnaoya@yahoo.co.jp>
2022-02-24 15:36:38 +0900

    new code

--- test/practice1.rb
+++ test/practice1.rb
@@ -1,7 +1,12 @@
 #p70 練習問題
 
+# Q1 2+3を計算して画面に表示してください
 puts 2 + 3
+
+# Q2 半径2cmの円の面積を求めてください。半径*半径*3.14
 puts 2 * 2 * 3.14
+
+# Q3 文字列"Ruby"を表示してください
 puts "Ruby"
 puts "abc" + "def"
 puts "123".to_i + "456".to_i
@@ -16,4 +21,13 @@ coffee = 400
 puts "コーヒー：#{coffee}円"
 puts "合計#{coffee+esp+esp}円"
 
+puts "Q8"
+coffee = 300
+total = coffee * 2
+p total
+tax = total * 0.08
+puts tax
+
+total = 300 * 2
+puts total
 
